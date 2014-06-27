6b7f413cac1a4d8d3259636360a9ac2451808c62
Dario Seminara <darios3@gmail.com>
2014-06-27 10:36:38 -0300

    simplificada implementacion reutilizando http-proxy

--- xploitProxy.js
+++ xploitProxy.js
@@ -5,65 +5,29 @@ var argyle = require('./lib/argyle/index.js');
 // este archivo NO esta en el repo
 var xploit = require('./xploit.js');
 
-
-var express = require('express')
-  , url = require('url');
-
-var path = require('path')
-  , url = require('url');
-
-var app = express();
-
+var httpProxy = require('http-proxy');
 var localPort = 8080;
 
-var proxy = function (urlString) {
-  var parsedUrl = url.parse(urlString);
-  var httpLib = parsedUrl.protocol === 'https:' ? 'https' : 'http';
-  var request = require(httpLib).request;
+// redirige todos los requests a facebook.com
+var proxy = httpProxy.createProxyServer({});
+var httpServer = require('http').createServer(function(req, res) {
+  console.log(req.headers);
+  proxy.web(req, res, { target: 'http://' + req.headers.host });
+});
 
-  return function (req, resp, next) {
-    var myReq = request({
-      hostname: parsedUrl.hostname,
-      port: parsedUrl.port,
-      path: parsedUrl.path,
-      method: req.method,
-      headers: req.headers,
-    }, function (myRes) {
-      resp.writeHead(myRes.statusCode, myRes.headers);
-      myRes.pipe(resp);
-    });
-    req.on('error', function(err) {
-      next(err);
-    });
-    req.pipe(myReq);
-  };
-};
-
-var multipleTargetProxy = function(req, resp, next) {
-};
-
-var messagePage = function(req, resp) {
-  resp.writeHead(200, {"Content-Type": "text/plain"});
-  resp.end("Esto no es un proxy todavia, es una prueba de que se puede interceptar requests\n");
-};
-
-app.get('/', messagePage);
-app.post('/', messagePage);
-
-app.listen(localPort);
-
+httpServer.listen(localPort, '0.0.0.0');
 
 // este es un proxy socks5 normal
-var server = argyle(8888, '0.0.0.0');
+var socksServer = argyle(8888, '0.0.0.0');
 
-server.hostPortFilter = function(host, port) {
-  if (xploit.target.host === host && xploit.target.port === port) {
-    return {host: '127.0.0.1', port: localPort}
-  }
+socksServer.hostPortFilter = function(host, port) {
+  //if (xploit.target.host === host && xploit.target.port === port) {
+    if (port === 80) return {host: '127.0.0.1', port: localPort}
+  //}
 };
 
 
-server.on('connected', function(req, dest) {
+socksServer.on('connected', function(req, dest) {
     req.pipe(dest);
     dest.pipe(req);
 });
