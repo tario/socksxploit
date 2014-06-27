bdfdd13f95b5d11a35a7cf8fa08bdce3d22ed23c
Dario Seminara <darios3@gmail.com>
2014-06-27 09:32:40 -0300

    proxy socks5 normal usando la libreria argyle

---
+++ normal.js
@@ -0,0 +1,11 @@
+// no se puede usar npm install como normalmente se haria
+// porque hay que modificar la libreria con nuestra propia version
+var argyle = require('./lib/argyle/index.js');
+
+// este es un proxy socks5 normal
+var server = argyle(8080, '127.0.0.1');
+server.on('connected', function(req, dest) {
+    req.pipe(dest);
+    dest.pipe(req);
+});
+
