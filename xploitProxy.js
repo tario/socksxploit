949de74da9eb8cdb568dfbce923de9ed313932c3
Dario Seminara <darios3@gmail.com>
2014-06-29 18:11:13 -0300

    agregado proxy para requests https

--- xploitProxy.js
+++ xploitProxy.js
@@ -1,18 +1,22 @@
 // no se puede usar npm install como normalmente se haria
 // porque hay que modificar la libreria con nuestra propia version
 var argyle = require('./lib/argyle/index.js');
+var http = require("http");
+var https = require("https");
+var fs = require("fs");
 
 // este archivo NO esta en el repo
 var xploit = require('./xploit.js');
 
 var httpProxy = require('http-proxy');
 var localPort = 8080;
+var httpsLocalPort = 8443;
 
 var lastSocksHost;
 
 // redirige todos los requests a facebook.com
 var proxy = httpProxy.createProxyServer({});
-var httpServer = require('http').createServer(function(req, res) {
+var httpServer = http.createServer(function(req, res) {
   console.log(req.headers);
 
   var target;
@@ -23,16 +27,39 @@ var httpServer = require('http').createServer(function(req, res) {
   }
   proxy.web(req, res, { target: target });
 });
-
 httpServer.listen(localPort, '0.0.0.0');
+
+
+var options = {
+  key: fs.readFileSync('keys/ssl-server.key'),
+  cert: fs.readFileSync('keys/ssl-server.crt')
+};
+var httpsServer = https.createServer(options, function (req, res) {
+  console.log(req.headers);
+
+  var target;
+  if (req.headers.host) {
+    target = 'http://' + req.headers.host;
+  } else {
+    target = lastSocksHost;
+  }
+  proxy.web(req, res, { target: target });
+});
+httpsServer.listen(httpsLocalPort, '0.0.0.0');
 
 // este es un proxy socks5 normal
 var socksServer = argyle(8888, '0.0.0.0');
 
 socksServer.hostPortFilter = function(host, port) {
-  if (xploit.shouldIntercept(host, port)) {
+  var intercept = xploit.shouldIntercept(host, port);
+  if (intercept) {
     lastSocksHost = host;
-    return {host: '127.0.0.1', port: localPort}
+
+    if (intecept === "http") {
+      return {host: '127.0.0.1', port: localPort};
+    } else {
+      return {host: '127.0.0.1', port: httpsLocalPort};
+    }
   }
 };
 
