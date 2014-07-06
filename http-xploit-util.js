cf287074478770cc02a9d4166bb298293a07f6ab
Dario Seminara <darios3@gmail.com>
2014-07-06 12:42:52 -0300

    refactor: evitar acumulacion de buffer when no se necesita interceptar nada, esto influye en la descarga de archivos muy grandes on en conexiones stream que nunca terminan (websockets, eventos html5, etc...)

--- http-xploit-util.js
+++ http-xploit-util.js
@@ -4,40 +4,49 @@ module.exports = {
   wrapResponse: function(res, changeData, shouldIntercept) {
     var fullResponse = new Buffer([]);
     var resHeaders = {};
+    var shouldInterceptThisRequest = false;
+
+    var interceptWrite = function(data) {
+      fullResponse = Buffer.concat([fullResponse, data]);
+    };
+
+    var interceptEnd = function() {
+      if (shouldIntercept(resHeaders)) {
+        if (resHeaders["content-encoding"] === "gzip") {
+          zlib.gunzip(fullResponse, function(err, buffer) {
+            buffer = changeData(buffer);
+            zlib.gzip(buffer, function(err, compressed) {
+              res.write(compressed);
+              res.end();
+            });
+          });
+        } else {
+          fullResponse = changeData(fullResponse);
+          res.write(fullResponse);
+          res.end();
+        }
+      } else {
+        res.write(fullResponse);
+        res.end();
+      }
+
+    };
 
     return {
       setHeader: function(name, value) {
         resHeaders[name] = value;
+        if (shouldIntercept(resHeaders)) {
+          this.write = interceptWrite;
+          this.end = interceptEnd;
+        }
         res.setHeader(name, value);
       },
       writeHead: res.writeHead.bind(res),
       on: res.on.bind(res),
       once: res.once.bind(res),
       emit: res.emit.bind(res),
-      write: function(data) {
-        fullResponse = Buffer.concat([fullResponse, data]);
-      },
-      end: function() {
-        if (shouldIntercept(resHeaders)) {
-          if (resHeaders["content-encoding"] === "gzip") {
-            zlib.gunzip(fullResponse, function(err, buffer) {
-              buffer = changeData(buffer);
-              zlib.gzip(buffer, function(err, compressed) {
-                res.write(compressed);
-                res.end();
-              });
-            });
-          } else {
-            fullResponse = changeData(fullResponse);
-            res.write(fullResponse);
-            res.end();
-          }
-        } else {
-          res.write(fullResponse);
-          res.end();
-        }
-
-      },
+      write: res.write.bind(res),
+      end: res.end.bind(res),
       removeListener: res.removeListener.bind(res),
     };
   }
