caefc9ef31547c494d470f21b2273f3707418555
Dario Seminara <darios3@gmail.com>
2014-07-30 21:26:08 -0300

    arreglado error cuando la longitud en bytes del dato reemplazado es distinta, se debe modificar el header de content-length

--- http-xploit-util.js
+++ http-xploit-util.js
@@ -6,8 +6,17 @@ module.exports = {
     var resHeaders = {};
     var shouldInterceptThisRequest = false;
 
+    var writeHeadArgs;
+
     var interceptWrite = function(data) {
       fullResponse = Buffer.concat([fullResponse, data]);
+    };
+
+    var sendHeaders = function() {
+      for (var headerName in resHeaders) {
+        res.setHeader(headerName, resHeaders[headerName]);
+      }
+      res.writeHead(writeHeadArgs.statusCode, writeHeadArgs.reason, writeHeadArgs.headers);
     };
 
     var interceptEnd = function() {
@@ -16,20 +25,31 @@ module.exports = {
           zlib.gunzip(fullResponse, function(err, buffer) {
             buffer = changeData(buffer);
             zlib.gzip(buffer, function(err, compressed) {
+              resHeaders["content-length"] = compressed.length;
+              sendHeaders();
               res.write(compressed);
               res.end();
             });
           });
         } else {
           fullResponse = changeData(fullResponse);
+          resHeaders["content-length"] = fullResponse.length;
+                    console.log("actual content length: " + fullResponse.length);
+          sendHeaders();
           res.write(fullResponse);
           res.end();
         }
       } else {
+        resHeaders["content-length"] = fullResponse.length;
+        sendHeaders();
         res.write(fullResponse);
         res.end();
       }
 
+    };
+
+    var interceptWriteHead = function(statusCode, reason, headers) {
+      writeHeadArgs = {statusCode: statusCode, reason: reason, headers: headers};
     };
 
     return {
@@ -38,8 +58,13 @@ module.exports = {
         if (shouldIntercept(resHeaders)) {
           this.write = interceptWrite;
           this.end = interceptEnd;
+          this.writeHead = interceptWriteHead;
         }
+
         res.setHeader(name, value);
+        if (name === "content-length") {
+          console.log("original content length: " + value);
+        }
       },
       writeHead: res.writeHead.bind(res),
       on: res.on.bind(res),
